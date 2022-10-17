2550defc2cc94f72d8044bb06ab18d0710675899
Kankit40 <Kankit40@gmail.com>
2022-10-17 22:36:06 +0530

    Refactor: UC2 correction

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -2,24 +2,21 @@ package com.bridgelabz;
 
 public class EmpWage {
     public static void main(String[] args) {
-        //Use case 3 added the option of part time
+        //Use case 1
 
         System.out.println("Welcome to Employee wage");
 
-        int empCheck = (int)(Math.random() * 3); //Generating Random Number
+        int empCheck = (int)(Math.random() * 2);
         int wage = 0;
         int isFulltime = 1;
         int empHrs = 0;
         int empWageperHrs = 20;
-        int isPartTime = 2;
 
         if(empCheck==isFulltime){
-            System.out.println("The employee is doing full time");
+            System.out.println("The employee is present");
             empHrs = 8;
 
-        }else if(empCheck==isPartTime){
-            System.out.println("Employee is doing part time");
-            empHrs = 4;
+
         }else{
             System.out.println("Employee is Absent");
             empHrs = 0;
